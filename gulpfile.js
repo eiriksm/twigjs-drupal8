332e94134162dad87d321613f20b501918f2bd53
Eirik S. Morland <eirik@morland.no>
2015-01-04 17:37:22 +0100

    The start of this.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var livereload = require('gulp-livereload');
+
+gulp.task('scripts', function() {
+  browserify('./js/main.js', {
+      debug: true,
+      transform: 'brfs'
+    })
+    .bundle()
+    .pipe(source('js.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('build/**').on('change', livereload.changed);
+  gulp.watch(['js/**/*'], ['scripts']);
+});
